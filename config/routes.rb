33756ac65360a220f7916aa2fbd3f582f7a40229
Trond Stern <trond@vonstern.de>
2018-12-02 13:33:13 +0100

    build new and create action for match

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users, :controllers => { registrations: 'registrations' }
   get :my_profile, to: "users#my_profile"
+
   resources :conversations do
     resources :messages, only: :create
   end
   resources :events do
   end
-
   resource :profile, only: [:edit, :update]
+
+  resources :matches, only: [:new, :create]
+
 end
