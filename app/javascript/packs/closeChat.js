8f409546184911998c84f49050ffea036d49ff1e
Fritz Meyer <meyerfr@gmx.de>
2018-12-05 15:03:13 +0100

    update Chat

--- app/javascript/packs/closeChat.js
+++ app/javascript/packs/closeChat.js
@@ -2,9 +2,9 @@ function setUpQuit(){
   var chatQuit = document.querySelector('.quit-chat');
   var chatWindow = document.querySelector('.chat-window');
   var testId = document.getElementById('test');
+  var chat = document.getElementById('chat');
 
   chatQuit.addEventListener('click', function(){
-
     if (testId.classList.contains("test-id-closed")) {
       chatQuit.innerHTML = "x"
     }
@@ -13,6 +13,9 @@ function setUpQuit(){
     }
     testId.classList.toggle("test-id-closed");
     chatWindow.classList.toggle("chat-window-closed");
+    chat.classList.toggle("chat-closed");
+    chatQuit.classList.toggle("quit-chat-closed");
+
     // testId.innerHTML = "";
   })
 }
